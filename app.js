d8bd78fd7f6cd84d81ba75c8dcc2a11e68a52897
gilphilbert <gil.philbert@gmail.com>
2020-03-09 14:08:25 -0700

    rename core

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var ws = require('./ws')
 
 var app = express()
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-})
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/index.html');
+//})o
+app.use(express.static('public'))
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
