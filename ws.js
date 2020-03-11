1874425f08a338ee97de9ebad34f5e57e85ac329
gilphilbert <gil.philbert@gmail.com>
2020-03-11 00:34:41 -0700

    playback, better lists

--- ws.js
+++ ws.js
@@ -1,45 +1,77 @@
 async function setup() {
 
-  var WebSocketServer = require("ws").Server
-  var wss = await new WebSocketServer({ port: "8080" })
-
+  var WebSocket = require("ws")
   var mpdapi = require("mpd-api")
-  var mpd = await mpdapi.connect({ host: "localhost", port: 6600 })
 
+  const { mpd } = mpdapi
+  const { cmd, MPDError } = mpd
+  try {
+    mpdc = await mpdapi.connect({ host: "localhost", port: 6600 })
+  } catch (e) {
+    if (e.errno === MPDError.CODES.PERMISSION) {
+      console.log('no permission to connect, probably invalid/missing password')
+    }
+  }
+
+  var wss = await new WebSocket.Server({ port: "8080" })
+
+  function broadcast (event_name, event_data) {
+    var payload = JSON.stringify({event:event_name, data: event_data})
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload)
+      }
+    })
+  }
+
+  mpdc.on("system", (e) => {
+    switch(e) {
+      case "playlist":
+        mpdc.api.queue.info()
+          .then(d => broadcast("pushQueue", d))
+        break
+      case "player":
+        mpdc.api.status.get()
+          .then(d => broadcast("pushStatus", d))
+        break
+      default:
+        console.log("[MPD] Unknown State Change:" + e)
+    }
+  })
 
   wss.on("connection", function (ws) {
     var disp = new Dispatcher(ws)
 
     disp.bind("getStatus", function () {
-      mpd.api.status.get()
+      mpdc.api.status.get()
         .then(d => disp.send("pushStatus", d))
     })
 
     disp.bind("getStats", function () {
-      mpd.api.status.stats()
+      mpdc.api.status.stats()
         .then(d => disp.send("pushStats", d))
     })
 
     disp.bind("rescanDB", function () {
-      mpd.api.db.rescan()
+      mpdc.api.db.rescan()
         .then(d => disp.send("notification", d))
     })
 
     disp.bind("updateDB", function () {
-      mpd.api.db.update()
+      mpdc.api.db.update()
         .then(d => disp.send("notification", d))
     })
 
     disp.bind("getArtists", function () {
-      mpd.api.db.list("artist")
+      mpdc.api.db.list("artist")
         .then(async (d) => {
           mod = d.map(i => i.artist)
-          disp.send("pushArtists", mod)
+          disp.send("pushLibrary", { artists: mod })
         })
     })
 
     disp.bind("getAlbums", function () {
-      mpd.api.db.list("album", null, "albumartist")
+      mpdc.api.db.list("album", null, "albumartist")
 	.then((d) => {
           mod = d.reduce((arr, item) => {
             item.album.forEach((e) => {
@@ -49,31 +81,31 @@ async function setup() {
             return arr;
           }, []);
           mod.sort((a, b) => (a.title > b.title) ? 1 : -1)
-          disp.send("pushAlbums", mod)
+          disp.send("pushLibrary", { albums: mod })
         })
     })
 
-    disp.bind("getArtistAlbums", function (data) {
-      mpd.api.db.list("album", "albumartist" + data.artist)
-        .then(d => disp.send("pushList", d))
+    disp.bind("getAlbum", function (data) {
+      mpdc.api.db.find(`((album == "${data.title}") AND (albumartist == "${data.artist}"))`)
+        .then(d => disp.send("pushLibrary", { album: d }))
     })
 
-    disp.bind("getList", function (...data) {
-      mpd.api.db.list(data)
-        .then(d => disp.send("pushList", d))
+    disp.bind("getArtistAlbums", function (data) {
+      mpdc.api.db.list("album", `(albumartist == "${data.artist}")`)
+        .then(d => disp.send("pushLibrary", { albums: d }))
     })
 
     disp.bind("getMounts", function () {
-      mpd.api.mounts.list()
+      mpdc.api.mounts.list()
         .then((d) => disp.send("pushMounts", d))
     })
 
     disp.bind("addMount", function (data) {
       var point = data.path.substr(data.path.lastIndexOf("/")+1, data.path.length)
       var share = data.type + "://" + data.host + data.path
-      mpd.api.mounts.mount(point, share)
+      mpdc.api.mounts.mount(point, share)
 	.then(() => {
-          mpd.api.mounts.list()
+          mpdc.api.mounts.list()
             .then((d) => {
               disp.send("pushMounts", d)
             })
@@ -81,14 +113,50 @@ async function setup() {
     })
 
     disp.bind("unmount", function (data) {
-      mpd.api.mounts.unmount(data.mountpoint)
+      mpdc.api.mounts.unmount(data.mountpoint)
 	.then((resp) => {
-          mpd.api.mounts.list((d) => {
+          mpdc.api.mounts.list((d) => {
             disp.send("pushMounts", d)
           })
         })
     })
+
+    disp.bind("getQueue", function () {
+      mpdc.api.queue.info()
+        .then((d) => disp.send("pushQueue", d))
+    })
+
+    disp.bind("enqueue", function (data) {
+      mpdc.api.queue.addid(data.uri)
+    })
+
+    disp.bind("clearQueue", function () {
+      mpdc.api.queue.clear()
+    })
+
+    disp.bind("play", function () {
+      mpdc.api.playback.play()
+    })
+    disp.bind("pause", function () {
+      mpdc.api.playback.pause()
+    })
+    disp.bind("resume", function () {
+      mpdc.api.playback.resume()
+    })
+    disp.bind("stop", function () {
+      mpdc.api.playback.stop()
+    })
+    disp.bind("toggle", function () {
+      mpdc.api.playback.toggle()
+    })
+    disp.bind("prev", function () {
+      mpdc.api.playback.prev()
+    })
+    disp.bind("next", function () {
+      mpdc.api.playback.next()
+    })
   })
+
 }
 
 var Dispatcher = function (ws) {
