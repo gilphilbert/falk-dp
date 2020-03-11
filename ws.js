5098ad24f491922334ca99cf7bceba99a58d26b7
gilphilbert <gil.philbert@gmail.com>
2020-03-10 22:20:37 -0700

    removed bogus class

--- ws.js
+++ ws.js
@@ -1,70 +1,95 @@
-var WebSocketServer = require("ws").Server
-var wss = new WebSocketServer({ port: "8080" })
+async function setup() {
 
-var mpdapi = require("mpd-api")
-var api = null
+  var WebSocketServer = require("ws").Server
+  var wss = await new WebSocketServer({ port: "8080" })
 
-mpdapi.connect({ host: host, port: port })
-  .then((con) => {
-    api = null
+  var mpdapi = require("mpd-api")
+  var mpd = await mpdapi.connect({ host: "localhost", port: 6600 })
+
+
+  wss.on("connection", function (ws) {
+    var disp = new Dispatcher(ws)
+
+    disp.bind("getStatus", function () {
+      mpd.api.status.get()
+        .then(d => disp.send("pushStatus", d))
+    })
+
+    disp.bind("getStats", function () {
+      mpd.api.status.stats()
+        .then(d => disp.send("pushStats", d))
+    })
+
+    disp.bind("rescanDB", function () {
+      mpd.api.db.rescan()
+        .then(d => disp.send("notification", d))
+    })
+
+    disp.bind("updateDB", function () {
+      mpd.api.db.update()
+        .then(d => disp.send("notification", d))
+    })
+
+    disp.bind("getArtists", function () {
+      mpd.api.db.list("artist")
+        .then(async (d) => {
+          mod = d.map(i => i.artist)
+          disp.send("pushArtists", mod)
+        })
+    })
+
+    disp.bind("getAlbums", function () {
+      mpd.api.db.list("album", null, "albumartist")
+	.then((d) => {
+          mod = d.reduce((arr, item) => {
+            item.album.forEach((e) => {
+              var flat = { title: e.album, artist: item.albumartist }
+              arr.push(flat)
+            })
+            return arr;
+          }, []);
+          mod.sort((a, b) => (a.title > b.title) ? 1 : -1)
+          disp.send("pushAlbums", mod)
+        })
+    })
+
+    disp.bind("getArtistAlbums", function (data) {
+      mpd.api.db.list("album", "albumartist" + data.artist)
+        .then(d => disp.send("pushList", d))
+    })
+
+    disp.bind("getList", function (...data) {
+      mpd.api.db.list(data)
+        .then(d => disp.send("pushList", d))
+    })
+
+    disp.bind("getMounts", function () {
+      mpd.api.mounts.list()
+        .then((d) => disp.send("pushMounts", d))
+    })
+
+    disp.bind("addMount", function (data) {
+      var point = data.path.substr(data.path.lastIndexOf("/")+1, data.path.length)
+      var share = data.type + "://" + data.host + data.path
+      mpd.api.mounts.mount(point, share)
+	.then(() => {
+          mpd.api.mounts.list()
+            .then((d) => {
+              disp.send("pushMounts", d)
+            })
+        })
+    })
+
+    disp.bind("unmount", function (data) {
+      mpd.api.mounts.unmount(data.mountpoint)
+	.then((resp) => {
+          mpd.api.mounts.list((d) => {
+            disp.send("pushMounts", d)
+          })
+        })
+    })
   })
-
-class MPD {
-  constructor({ host, port }={}) {
-    host = host || "localhost"
-    port = port || 6600
-    mpdapi.connect({ host: host, port: port })
-    .then((con) => {
-      console.log('Connected to MPD')
-    con.api.db.list("album", "albumartist Eagles").then((d) => {
-      console.log(d)
-    })
-      this._con=con
-    })
-  }
-
-  status (callback) {
-    this._con.api.status.get()
-      .then(callback)
-  }
-  stats (callback) {
-    this._con.api.status.stats()
-      .then(callback)
-  }
-
-  rescandb (callback) {
-    this._con.api.db.rescan()
-      .then(callback)
-  }
-  updatedb (callback) {
-    this._con.api.db.update()
-      .then(callback)
-  }
-
-  listdb ({ tag, filter, group }={}, callback) {
-    if (!filter && !group) {
-      this._con.api.db.list(tag).then(callback)
-    } else if (!group) {
-      this._con.api.db.list(tag, filter).then(callback)
-    } else {
-      this._con.api.db.list(tag, null, group).then(callback)
-    }
-  }
-
-  listmounts (callback) {
-    this._con.api.mounts.list()
-      .then(callback)
-  }
-  addmount ({ mountpoint, share }={}, callback) {
-    this._con.api.mounts.mount(mountpoint, share)
-      .then(callback)
-  }
-  unmount (data, callback) {
-    this._con.api.mounts.unmount(data)
-      .then(callback)
-  }
 }
-var mpd = new MPD({ host: "localhost", port: 6600 })
 
 var Dispatcher = function (ws) {
   var callbacks = {}
@@ -96,92 +121,4 @@ var Dispatcher = function (ws) {
   }
 }
 
-wss.on("connection", function (ws) {
-  var disp = new Dispatcher(ws)
-
-  disp.bind("getStatus", function() {
-    mpd.status((d) => {
-      disp.send("pushStatus", d)
-    })
-  })
-
-  disp.bind("getStats", function() {
-    mpd.stats((d) => {
-      disp.send("pushStats", d)
-    })
-  })
-
-  disp.bind("rescanDB", function() {
-    mpd.rescandb((d) => {
-      disp.send("notification", d)
-    })
-  })
-
-  disp.bind("updateDB", function() {
-    mpd.updatedb((d) => {
-      disp.send("notification", d)
-    })
-  })
-
-  disp.bind("getArtists", function() {
-    mpd.listdb({ tag: "artist" }, async (d) => {
-      mod = d.map(i => i.artist)
-      disp.send("pushArtists", mod)
-    })
-  })
-
-  disp.bind("getAlbums", function() {
-    mpd.listdb({ tag: "album", group: "albumartist" }, (d) => {
-      mod = d.reduce((arr, item) => {
-        item.album.forEach((e) => {
-          var flat = { title: e.album, artist: item.albumartist }
-          arr.push(flat)
-        })
-        return arr;
-      }, []);
-      mod.sort((a, b) => (a.title > b.title) ? 1 : -1)
-      disp.send("pushAlbums", mod)
-    })
-  })
-
-  disp.bind("getArtistAlbums", function(data) {
-    mpd.listdb({ tag: "album", filter: "albumartist Eagles" }, (d) => {
-      disp.send("pushList", d)
-    }) 
-  })
-
-  disp.bind("getList", function(data) {
-    mpd.listdb(data, (d) => {
-      disp.send("pushList", d)
-    })
-  })
-
-  disp.bind("getMounts", function() {
-    mpd.listmounts((d) => {
-      disp.send("pushMounts", d)
-    })
-  })
-
-  disp.bind("addMount", function (data) {
-    var point = data.path.substr(data.path.lastIndexOf("/")+1, data.path.length)
-    var shr = data.type + "://" + data.host + data.path
-    var payload = { mountpoint: point, share: shr }
-    mpd.addmount(payload, (resp) => {
-      mpd.listmounts((d) => {
-        disp.send("pushMounts", d)
-      })
-    })
-  })
-  disp.bind("unmount", function (data) {
-    mpd.unmount(data.mountpoint, (resp) => {
-      mpd.listmounts((d) => {
-        disp.send("pushMounts", d)
-      })
-    })
-  })
-/*
-        mpdui.mounts.add(data, (d) => {
-          ws.send(formatMessage("mounts", d))
-	})
-*/
-})
+setup()
