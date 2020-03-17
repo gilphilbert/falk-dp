3dc02fa590175238ba49a37010f4170d54ba2f08
gilphilbert <gil.philbert@gmail.com>
2020-03-17 11:59:44 -0700

    branding changes

--- ui/js/src/build.sh
+++ ui/js/src/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uglifyjs app.js datatools.js domBuilder.js router.js uitools.js viosocket.js -c -m -o ../moosic.min.js
+uglifyjs app.js datatools.js domBuilder.js router.js uitools.js websocket.js -c -m -o ../moosic.min.js
