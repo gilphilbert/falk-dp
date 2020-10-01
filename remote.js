0f88d6d47380d21b4a6b4a0cff5f42e8a9d4106f
gilphilbert <gil.philbert@gmail.com>
2020-09-30 21:19:56 -0700

    monitor for hid

--- remote.js
+++ remote.js
@@ -1,7 +1,14 @@
+var HID = require('node-hid')
+var udev = require("udev")
+
+var device;
+
 async function setup () {
+    checkDevices()
+    startMonitor()
+}
 
-    var HID = require('node-hid');
-
+function checkDevices() {
     //satechi remote
     vendorId = 1444;
     productId = 4096;
@@ -13,67 +20,81 @@ async function setup () {
     });
 
     if (deviceInfo) {
-        var mpdapi = require('mpd-api')
-        var mpdc = null
-        async function connectMPD () {
-            console.log('Connecting to MPD...')
-            try {
-                mpdc = await mpdapi.connect({ path: '/run/mpd/socket' })
-                console.log('Connected to MPD (remote)')
-
-                mpdc.on('close', () => {
-                console.log('MPD connection lost')
-                connectMPD()
-                })
-            } catch (e) {
-                console.log('Couldn\'t connect to MPD')
-                console.log(e)
-            }
-        }
-        await connectMPD()
-
-        var device = new HID.HID( deviceInfo.path );
-        device.on("data", function(data) {
-            val = data.readUInt32LE();
-            if (val === 1 || val === 2) { return; }
-            cmd = "";
-            switch(val) {
-                case 131074:
-                    cmd = "volUp";
-                    break;
-                case 262146:
-                    cmd = "volDown";
-                    break;
-                case 524290:
-                    cmd = "mute";
-                    break;
-                case 32770:
-                    // prev
-                    mpdc.api.playback.prev()
-                    break;
-                case 65538:
-                    // next
-                    mpdc.api.playback.next()
-                    break;
-                case 8194:
-                    // pausePlay
-                    mpdc.api.playback.toggle()
-                    break;
-                case 258:
-                    cmd = "home";
-                    break;
-            }
-            if (cmd != "") {
-                console.log(cmd);
-            } else {
-                console.log(val);
-            }
-        });
-
-        device.on("error", function(err) {
-            console.log(err);
-        });
+        openDevice(deviceInfo)
     }
+}
+
+function startMonitor() {
+    var monitor = udev.monitor("hidraw")
+    monitor.on('add', function (device) {
+        checkDevices()
+    })
+}
+
+async function openDevice(deviceInfo) {
+    var mpdapi = require('mpd-api')
+    var mpdc = null
+    async function connectMPD () {
+        console.log('Connecting to MPD...')
+        try {
+            mpdc = await mpdapi.connect({ path: '/run/mpd/socket' })
+            console.log('Connected to MPD (remote)')
+
+            mpdc.on('close', () => {
+            console.log('MPD connection lost')
+            connectMPD()
+            })
+        } catch (e) {
+            console.log('Couldn\'t connect to MPD')
+            console.log(e)
+        }
+    }
+    await connectMPD()
+
+    device = new HID.HID(deviceInfo.path);
+    device.on("data", function(data) {
+        val = data.readUInt32LE();
+        if (val === 1 || val === 2) { return }
+        cmd = "";
+        switch(val) {
+            case 131074:
+                cmd = "volUp";
+                break;
+            case 262146:
+                cmd = "volDown";
+                break;
+            case 524290:
+                cmd = "mute";
+                break;
+            case 32770:
+                // prev
+                mpdc.api.playback.prev()
+                break;
+            case 65538:
+                // next
+                mpdc.api.playback.next()
+                break;
+            case 8194:
+                // pausePlay
+                mpdc.api.playback.toggle()
+                break;
+            case 258:
+                cmd = "home";
+                break;
+        }
+        if (cmd != "") {
+            console.log(cmd);
+        } else {
+            console.log(val);
+        }
+    });
+
+    device.on("error", function(err) {
+        console.log(err)
+        device.disconnect()
+        mpdapi.disconnect()
+        device = null
+    });
 }
 
 module.exports = {
